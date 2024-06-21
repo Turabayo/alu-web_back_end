0aea42192318f0b7b9ea8a45bfb9cb0b8a7b8d65
Turabayo <i.turabayo@alustudent.com>
2024-06-21 11:57:59 +0000

    users

--- user.py
+++ user.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python3
-"""
-Declare a SQLAlchemy model named 'User' corresponding to a
-database table named "users"
-"""
+"""Archemy User class"""
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-)
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
-    """
-    Definition of class User
-    """
+    """User class"""
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
